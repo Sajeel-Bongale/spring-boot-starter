939acab2a4f6ad9ee846c59940ac31f164437a5d
Sajeel-Bongale <sajeelbongale@gmail.com>
2023-01-11 09:38:13 +0530

    Adds global exception handling to manage field validations consistently

--- src/main/java/com/clairvoyant/employees/Employee.java
+++ src/main/java/com/clairvoyant/employees/Employee.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,11 +22,13 @@ public class Employee {
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private Integer id;
 
-    @NotNull
+    @Pattern(regexp = "^[A-Za-z]+$", message = "First Name Cannot contain special characters")
+    @NotNull(message = "First Name must not be null")
     @Size(min = 2, message = "First Name must have at least 2 characters")
     private String firstName;
 
-    @NotNull
+    @Pattern(regexp = "^[A-Za-z]+$", message = "Last Name Cannot contain special characters")
+    @NotNull(message = "Last Name must not be null")
     @Size(min = 2, message = "Last Name must have at least 2 characters")
     private String lastName;
 
---
+++ src/main/java/com/clairvoyant/employees/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.clairvoyant.employees;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+//        return new ResponseEntity<>(getErrorsMap(errors), HttpStatus.BAD_REQUEST);
+        String firstErrorMessage = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return new ResponseEntity<>(firstErrorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
+    }
+}
