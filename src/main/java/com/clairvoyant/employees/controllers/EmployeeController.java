4487a836be8acfe5ac241f26a0d1a2408d7d8360
Sajeel-Bongale <sajeelbongale@gmail.com>
2023-01-12 16:18:21 +0530

    Adds log4j logger to the application

--- src/main/java/com/clairvoyant/employees/controllers/EmployeeController.java
+++ src/main/java/com/clairvoyant/employees/controllers/EmployeeController.java
@@ -3,9 +3,10 @@ package com.clairvoyant.employees.controllers;
 import com.clairvoyant.employees.dto.EmployeeDto;
 import com.clairvoyant.employees.dto.EmployeeUpdateDto;
 import com.clairvoyant.employees.model.Employee;
-import com.clairvoyant.employees.respository.EmployeeRepository;
 import com.clairvoyant.employees.service.EmployeeService;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,13 +17,12 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(path = "/api/employee")
 @Validated
 public class EmployeeController {
+    private static final Logger logger = LogManager.getLogger(EmployeeController.class);
 
     @Autowired
     private ModelMapper modelMapper;
@@ -30,45 +30,52 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @Autowired
-    EmployeeRepository employeeRepository;
-
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> addEmployee(@Valid @RequestBody EmployeeDto employeeDto) {
+        logger.info("Received creation request for employee addition");
         Employee employee = this.modelMapper.map(employeeDto, Employee.class);
         this.employeeService.addEmployee(employee);
+        logger.info("Employee with email {} created successfully", employeeDto.getEmail());
         return new ResponseEntity<>("Saved Employee", HttpStatus.CREATED);
     }
 
     @GetMapping()
     public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        logger.info("Received request for getting all employees");
         List<Employee> employees = this.employeeService.getAllEmployees();
         List<EmployeeDto> employeeDtoList = employees
                 .stream()
                 .map(employee -> this.modelMapper.map(employee, EmployeeDto.class))
-                .collect(Collectors.toList());
+                .toList();
+        logger.info("Sending employee list of size {}", employeeDtoList.size());
         return new ResponseEntity<>(employeeDtoList, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{email}")
     public ResponseEntity<EmployeeDto> getEmployeeByEmail(@PathVariable String email) {
+        logger.info("Received request for getting employee with email {}", email);
         Employee employee = this.employeeService.getEmployeeByEmail(email);
         EmployeeDto employeeDto = this.modelMapper.map(employee, EmployeeDto.class);
+        logger.info("Sending employee details with email {}", email);
         return new ResponseEntity<>(employeeDto, HttpStatus.OK);
     }
 
     @PutMapping(path = "/{email}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> updateEmployee(@Valid @RequestBody EmployeeUpdateDto employeeUpdateDto,
                                                  @PathVariable String email) {
+        logger.info("Received request for updating employee with email {}", email);
         Employee employee = this.employeeService.getEmployeeByEmail(email);
         this.modelMapper.map(employeeUpdateDto, employee);
         this.employeeService.updateEmployee(employee);
+        logger.info("Updated employee with email {}", email);
         return new ResponseEntity<>("Updated Employee with email " + email, HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{email}")
     public ResponseEntity<String> deleteEmployeeByEmail(@PathVariable String email) {
+        logger.info("Received request for deleting employee with email {}", email);
         this.employeeService.deleteEmployeeByEmail(email);
+        logger.info("Deleted employee with email {}", email);
         return new ResponseEntity<>("Deleted employee with email " + email, HttpStatus.NOT_FOUND);
     }
 }
